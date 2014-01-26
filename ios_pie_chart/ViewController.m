a6af245af4f5255cec5c4e34e8a46e89ac6dc2cf
Maxim Bilan <maximb.mail@gmail.com>
2014-01-26 21:01:50 +0200

    implemented sample code

--- ios_pie_chart/ViewController.m
+++ ios_pie_chart/ViewController.m
@@ -8,7 +8,15 @@
 
 #import "ViewController.h"
 
+#import "Chart.h"
+#import "ChartData.h"
+#import "LegendView.h"
+
+#define RAND_FROM_TO(min,max) (min + arc4random_uniform(max - min + 1))
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet Chart *chart;
 
 @end
 
@@ -17,13 +25,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    
+    float total;
+    for (NSInteger groupIndex = 0; groupIndex < 5; ++groupIndex) {
+        
+        [array removeAllObjects];
+        total = 0.0;
+        for (NSInteger itemIndex = 0; itemIndex < 10; ++itemIndex) {
+            ChartItem *item = [[ChartItem alloc] init];
+            item.name = [NSString stringWithFormat:@"Item %d", itemIndex];
+            item.value = RAND_FROM_TO(1, 500);
+            total += item.value;
+            [array addObject:item];
+        }
+        
+        [self.chart setData:array withKey:[NSString stringWithFormat:@"Group %d", groupIndex+1] withTotal:total];
+    }
 }
 
 @end
